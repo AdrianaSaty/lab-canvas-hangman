8698852598c986cff55a2ccd79b7b6d81a80e905
Adriana Saty Bertelli <adri-saty@hotmail.com>
2019-07-15 21:05:47 -0300

    oi

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,32 +1,87 @@
+class HangmanCanvas {
+  constructor(secretWord){
+    this.ctx = document.getElementById('hangman').getContext('2d');
+    this.ctx.clearRect(0, 0, 1000, 600);
+    this.createBoard()
+    this.drawLines(secretWord);
+  }
+  createBoard(){
+    this.ctx.beginPath(); //triangle   
+    this.ctx.moveTo(50, 500);
+    this.ctx.lineTo(200, 500);
+    this.ctx.moveTo(200, 500);
+    this.ctx.lineTo(125, 400);
+    this.ctx.moveTo(125, 400);
+    this.ctx.lineTo(50, 500);
+    this.ctx.stroke();
+    this.ctx.closePath();
+    this.ctx.beginPath(); //hangman structure   
+    this.ctx.moveTo(125, 400);
+    this.ctx.lineTo(125, 100);
+    this.ctx.lineTo(400, 100);
+    this.ctx.lineTo(400, 150);
+    this.ctx.stroke();
+    this.ctx.closePath();
+    this.ctx.beginPath(); //small circle 
+    this.ctx.arc(400, 180, 30, 0, Math.PI*2); // ctx.arc(x, y, radius, startAngle, endAngle)
+    this.ctx.stroke();
+    this.ctx.beginPath(); //body   
+    this.ctx.moveTo(400, 210);
+    this.ctx.lineTo(400, 320);  
+    this.ctx.stroke();
+    this.ctx.closePath();
+    this.ctx.beginPath(); //left arm   
+    this.ctx.moveTo(400, 250);
+    this.ctx.lineTo(340, 220);  
+    this.ctx.stroke();
+    this.ctx.closePath();
+    this.ctx.beginPath(); //right arm   
+    this.ctx.moveTo(400, 250);
+    this.ctx.lineTo(460, 220);  
+    this.ctx.stroke();
+    this.ctx.closePath();
+    this.ctx.beginPath(); //left leg   
+    this.ctx.moveTo(400, 320);
+    this.ctx.lineTo(460, 380);  
+    this.ctx.stroke();
+    this.ctx.closePath();
+    this.ctx.beginPath(); //right leg   
+    this.ctx.moveTo(400, 320);
+    this.ctx.lineTo(340, 380);  
+    this.ctx.stroke();
+    this.ctx.closePath();
+  }
 
-function HangmanCanvas(secretWord) {
-  this.ctx = document.getElementById('hangman').getContext('2d');
-}
+  drawLines(secretWord){
+    let x = 250;
+    console.log(secretWord);
+    for(let i =  0; i < secretWord.length; i += 1){
+      this.ctx.beginPath();
+      this.ctx.moveTo(x, 500);
+      x += 50;
+      this.ctx.lineTo(x, 500);
+      x += 30;
+      this.ctx.stroke();
+      this.ctx.closePath();
+    }
+  }
 
-HangmanCanvas.prototype.createBoard = function () {
+  writeCorrectLetter(){
+ 
+  }
+  writeWrongLetter(){
+ 
+  }
+  drawHangman(){
+ 
+  }
+  gameOver(){
+ 
+  }
+  winner(){
+ 
+  }
+ }
 
-};
 
-HangmanCanvas.prototype.drawLines = function () {
-
-};
-
-HangmanCanvas.prototype.writeCorrectLetter = function (index) {
-
-};
-
-HangmanCanvas.prototype.writeWrongLetter = function (letter, errorsLeft) {
-
-};
-
-HangmanCanvas.prototype.drawHangman = function (shape) {
-
-};
-
-HangmanCanvas.prototype.gameOver = function () {
-
-};
-
-HangmanCanvas.prototype.winner = function () {
-
-};
+ 
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,42 +1,70 @@
 var hangman;
+let listWords = ['INRONHACK', 'PIZZA', 'REACT']
+class Hangman {
+  constructor(listWords) {
+    this.words = listWords;
+    this.secretWord = '';
+    this.letters = []; //array with letters already clicked, not repetead
+    this.guessedLetter = ''; //string with correct letters already cliked
+    this.errorsLeft = 6;
+    this.hangmanCanvas = new HangmanCanvas(this.getWord());
+    this.img = new Image();
+  }
+  getWord(){
+    return this.secretWord = this.words[Math.floor(Math.random() * this.words.length)];
+  }
 
-// function Hangman() {
+  checkIfLetter(letter){
+    return letter > 64 && letter < 91;
+  }
 
-// }
+  checkClickedLetters(letter) {
+    return this.secretWord.includes( (`${letter}`) ) ? true : false;
+  }
 
-// Hangman.prototype.getWord = function () {
+  addCorrectLetter(letter){
+    this.guessedLetter += letter;
+    this.letters.push(letter);
+    console.log(this.guessedLetter)
 
-// };
+  }
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+  addWrongLetter(letter){
+    this.letters.push(letter);
+    this.errorsLeft --;
+    if( this.errorsLeft === 0){
+      console.log('cabo')
+      this.checkGameOver();
+    }
+    console.log(this.letters)
+  }
 
-// };
+  checkGameOver(){
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+    window.onload = function() {
+      var c = document.getElementById("hangman");
+      var ctx = c.getContext("2d");
+      this.img.src = "/starter_code/images/gameover.png"
+      ctx.drawImage(img, 10, 10);
+    }
+  }
 
-// };
+  checkWinner() {
+    this.guessedLetter.length === this.secretWord.length ? true : false;
+    console.log('ganhou')
+}
 
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+}
 
 document.getElementById('start-game-button').onclick = function () {
-  hangman = new Hangman();
+  hangman = new Hangman(listWords);
 };
 
 
 document.onkeydown = function (e) {
+  if(!hangman.checkIfLetter(e.keyCode)) return alert('This is not a letter!');
+  hangman.checkClickedLetters(e.key) ? hangman.addCorrectLetter(e.key) : hangman.addWrongLetter(e.key);
+
 
 };
+0
